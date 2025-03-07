e7e5a1d39bcfe3c6fc5e10f65be49c6b6f9bd0f7
MartyTheGout <chihyung0@naver.com>
2025-03-08 00:05:35 +0900

    [View] CollectionListRegularForm added, for unified format inside collectionView, also in the header

---
+++ BetOnCrypto/View-ViewModel/Market/CollectionListRegularForm.swift
@@ -0,0 +1,24 @@
+//
+//  CollectionRegularForm.swift
+//  BetOnCrypto
+//
+//  Created by marty.academy on 3/7/25.
+//
+
+import UIKit
+/**
+ 
+ This, "CollectionRegularForm"  provide  regular-consistent spacing in header, also in collection column record.
+ */
+final class CollectionListRegularForm : UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        axis = .horizontal
+        distribution = .equalSpacing
+        spacing = 10
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
