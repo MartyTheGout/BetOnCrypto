ea2518dc2094de01598a2c5836d8689435d31544
MartyTheGout <chihyung0@naver.com>
2025-03-08 19:10:53 +0900

    [View Design] bottom line under navigation bar is added

--- BetOnCrypto/View-ViewModel/Search/SearchViewController.swift
+++ BetOnCrypto/View-ViewModel/Search/SearchViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 final class SearchViewController : BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#function, String(describing: self))
+        configureNavigationTitleView()
     }
+    
+    private func configureNavigationTitleView() {
+        let title = UILabel()
+        title.text = "가상자산 / 심볼검색"
+        title.font = .boldSystemFont(ofSize: 17)
+
+        let spacer = UIView()
+        let divider = Divider()
+        
+        let stack = UIStackView(arrangedSubviews: [title, spacer])
+        stack.axis = .horizontal
+
+        stack.snp.makeConstraints {
+            $0.width.greaterThanOrEqualTo(500).priority(.low)
+        }
+        
+        navigationItem.titleView = stack
+        
+        navigationController?.navigationBar.addSubview(divider)
+        divider.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-1)
+        }
+    }
+
     
     override func configureViewDetails() {
         view.backgroundColor = .white
