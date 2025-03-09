f42dfa46085436e75fabf1415e77798a99027b45
MartyTheGout <chihyung0@naver.com>
2025-03-09 13:28:59 +0900

    [Repository] added Converter in TrendingRepository

--- BetOnCrypto/DataLayer/TrendingDataRepository.swift
+++ BetOnCrypto/DataLayer/TrendingDataRepository.swift
@@ -9,24 +9,52 @@ import Foundation
 import Alamofire
 
 final class TrendingDataRepository {
-//    func getTrendingData(dataFeeder : @escaping (_ with: [MarketPresentable]) -> Void) {
-//        NetworkManager.shared.callRequest(ExchangeRouter.ticker) { [weak self] (result: Result<[MarketData], AFError>) in
-//            switch result {
-//            case .success(let marketData) :
-//                let convertedData: [MarketPresentable] = marketData.map {
-//                    self?.convertOriginToPresentable(with: $0) ?? MarketPresentable.mock
-//                }
-//                dataFeeder(convertedData)
-//            case .failure(let error): print(error)
-//            }
-//        }
-//    }
+    func getTrendingData(dataFeeder : @escaping (_ with: TrendingResponse) -> Void) {
+        NetworkManager.shared.callRequest(ExchangeRouter.ticker) { [weak self] (result: Result<TrendingResponse, AFError>) in
+            switch result {
+            case .success(let trendingData) :
+                print(trendingData)
+            case .failure(let error): print(error)
+            }
+        }
+    }
     
-    func getTrendingMock(dataFeeder: @escaping (_ with: MockedSearchResult) -> Void) {
-        dataFeeder(mockedSearchResult)
+    func getTrendingMock(dataFeeder: @escaping ([TrendingCoinPresentable], [TrendingNFTPresentable]) -> Void) {
+        let convertedCoin = convertOriginToPresentableCoin(originalData: mockedTrendingResponse.coins)
+        let convertedNFT = convertOriginToPresentableNFT(originalData: mockedTrendingResponse.nfts)
+        
+        dataFeeder(convertedCoin, convertedNFT)
     }
 }
 
 extension TrendingDataRepository {
+    private func convertOriginToPresentableCoin(originalData: [TrendingCoinItem]) -> [TrendingCoinPresentable] {
+        let converted = originalData.map {
+            let coinDetail = $0.item
 
+            let percetageKRW = coinDetail.data.priceChangePercentage24h["krw"]!
+            
+            return TrendingCoinPresentable(
+                name: coinDetail.name,
+                symbol: coinDetail.symbol,
+                thumb: coinDetail.thumb,
+                data: TrendingCoinDataPresentable(krw: NumberFormatManager.shared.getDecialWithMax2FD(originalValue: percetageKRW) + "%")
+            )
+        }
+        
+        return converted
+    }
+    
+    private func convertOriginToPresentableNFT(originalData: [TrendingNFTItem]) -> [TrendingNFTPresentable] {
+        let converted = originalData.map {
+            
+            return TrendingNFTPresentable(
+                name: $0.name,
+                thumb: $0.thumb,
+                floorPrice24hPercentageChange: NumberFormatManager.shared.getDecialWithMax2FD(originalValue: $0.floorPrice24hPercentageChange),
+                data: TrendingNFTDataPresentable(floorPrice: $0.data.floorPrice))
+        }
+        
+        return converted
+    }
 }
